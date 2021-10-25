2ecda1611b77c2ca83ede3ba72282852ce437d1e
Thomas Chen <z5313390@ad.unsw.edu.au>
2021-10-26 10:04:57 +1100

    Created sample entities files

---
+++ src/main/java/dungeonmania/entities/Entity.java
@@ -0,0 +1,15 @@
+package dungeonmania.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+import dungeonmania.util.Position;
+
+
+public class Entity {
+
+    public Entity(String entityId, String entityType, Position position) {
+
+    }
+
+
+}
