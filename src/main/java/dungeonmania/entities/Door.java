80e2033416e76e2ca203da4c15a4e3ab5edee657
Thomas Chen <z5313390@ad.unsw.edu.au>
2021-10-27 19:38:38 +1100

    fixing typo

--- src/main/java/dungeonmania/entities/Door.java
+++ src/main/java/dungeonmania/entities/Door.java
@@ -3,7 +3,7 @@ package dungeonmania.entities;
 import dungeonmania.util.Position;
 
 public class Door extends StaticEntity {
-    public Door(Position position, String type, String ID, boolean isInteractable, String colour) {
-        super(position,type, ID, isInteractable, colour);
+    public Door(Position position, String type, String ID, boolean isInteractable) {
+        super(position,type, ID, isInteractable);
     } 
 }
