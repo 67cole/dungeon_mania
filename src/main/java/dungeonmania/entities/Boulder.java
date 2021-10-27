18401e354fdcf578713af0970a3f2e57eeacb308
Winston Liang <liang.w0831@gmail.com>
2021-10-27 17:40:14 +1100

    Cleaning up snake case

--- src/main/java/dungeonmania/entities/Boulder.java
+++ src/main/java/dungeonmania/entities/Boulder.java
@@ -3,7 +3,7 @@ package dungeonmania.entities;
 import dungeonmania.util.Position;
 
 public class Boulder extends StaticEntity{
-    public Boulder(Position position, String type, String ID, boolean IsInteractable, String colour) {
-        super(position,type, ID, IsInteractable, colour);
+    public Boulder(Position position, String type, String ID, boolean isInteractable, String colour) {
+        super(position,type, ID, isInteractable, colour);
     }   
 }
