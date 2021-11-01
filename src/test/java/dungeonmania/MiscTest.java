1530485b27941ee6530d705711cde647da9c6301
Colin Ngo <z5310745@ad.unsw.edu.au>
2021-11-01 14:48:03 +1100

    fixed up exceptions for tick function

--- src/test/java/dungeonmania/MiscTest.java
+++ src/test/java/dungeonmania/MiscTest.java
@@ -1,14 +1,33 @@
 package dungeonmania;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+import dungeonmania.DungeonManiaController;
+import dungeonmania.response.models.DungeonResponse;
+import dungeonmania.response.models.EntityResponse;
+import dungeonmania.exceptions.*;
+import dungeonmania.util.Direction;
+import dungeonmania.util.Position;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 
 public class MiscTest {
-    @Test
-    public void testDungeons() {
-        assertTrue(DungeonManiaController.dungeons().size() > 0);
-        assertTrue(DungeonManiaController.dungeons().size() > -1);
-        assertTrue(DungeonManiaController.dungeons().contains("maze"));
+
+    /**
+     * Helper function that returns true if the entity type is on the same position
+     * as whats given in the param 
+     * @param pos
+     * @param main
+     * @param type
+     * @return boolean
+     */
+    public boolean entityInPlace(Position pos, DungeonResponse main, String type) {
+        for (EntityResponse entity: main.getEntities()) {
+            if (entity.getPosition().equals(pos) && entity.getType().equals(type)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
