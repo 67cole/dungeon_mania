ce1f73cd208dd8f402ffdefccca37253b8623f56
Colin Ngo <z5310745@ad.unsw.edu.au>
2021-11-01 12:55:56 +1100

    fixed up some test cases for moving entity as they were failing

--- src/test/java/dungeonmania/MovingEntityTest.java
+++ src/test/java/dungeonmania/MovingEntityTest.java
@@ -9,10 +9,29 @@ import dungeonmania.util.Direction;
 import dungeonmania.util.Position;
 
 public class MovingEntityTest {
+
+    // Checks the movement of the player
+    @Test 
+    public void testPlayerMovement() {
+        DungeonManiaController controller = new DungeonManiaController();
+        DungeonResponse newDungeon = controller.newGame("advanced", "Peaceful");
+        Position toBePos = new Position(1,1);
+        assertTrue(entityInPlace(toBePos, newDungeon, "player"));
+
+        DungeonResponse tickHolder = null;
+
+        tickHolder =  controller.tick(null, Direction.DOWN);
+        Position moveDown = new Position(1,2);
+        assertTrue(entityInPlace(moveDown, tickHolder, "player"));
+
+        tickHolder =  controller.tick(null, Direction.RIGHT);
+        Position moveRight = new Position(2,2);
+        assertTrue(entityInPlace(moveRight, tickHolder, "player"));
+    }
+    
     
     // Check if newGame imports the json data properly and adds everything into the entitieslist
     @Test
-
     public void testMercenarySpawn() {
         DungeonManiaController controller = new DungeonManiaController();
         DungeonResponse newDungeon = controller.newGame("advanced", "Peaceful");
@@ -26,6 +45,52 @@ public class MovingEntityTest {
         assertTrue(entityInPlace(toBePos, tickHolder, "mercenary"));
 
     }
+
+    // Check if zombie spawns in every 20 tick on Standard Difficulty
+    @Test
+    public void testZombieSpawnStandard() {
+        DungeonManiaController controller = new DungeonManiaController();
+        DungeonResponse newDungeon = controller.newGame("testRoom", "Standard");
+        Position toBePos = new Position(1,1);
+        assertTrue(entityInPlace(toBePos, newDungeon, "player"));
+        DungeonResponse tickHolder = null;
+        
+        for (int i = 0; i < 20; i++) {
+            tickHolder =  controller.tick(null, Direction.DOWN);
+        }
+
+        Position zombieSpawn = new Position(3,3);
+
+        assertTrue(entityInPlace(zombieSpawn, tickHolder, "zombie_toast"));
+    }
+
+    // Check if zombie spawns in every 15 tick on Hard Difficulty
+    @Test
+    public void testZombieSpawnHard() {
+        DungeonManiaController controller = new DungeonManiaController();
+        DungeonResponse newDungeon = controller.newGame("testRoom", "Hard");
+        Position toBePos = new Position(1,1);
+        assertTrue(entityInPlace(toBePos, newDungeon, "player"));
+        DungeonResponse tickHolder = null;
+        
+        for (int i = 0; i < 15; i++) {
+            tickHolder =  controller.tick(null, Direction.DOWN);
+        }
+
+        Position zombieSpawn = new Position(3,3);
+
+        assertTrue(entityInPlace(zombieSpawn, tickHolder, "zombie_toast"));
+    }
+
+
+
+
+
+
+
+
+
+
     /**
      * Helper function that returns true if the entity type is on the same position
      * as whats given in the param 
